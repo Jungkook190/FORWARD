b9df16d912f5c66c64927c7591379831d80f009d
anagha <103480559+Jungkook190@users.noreply.github.com>
2024-09-11 15:33:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "22620068")
-    API_HASH = os.environ.get("API_HASH", "11e2c113078324f7e36688baa86c3911")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7025776766:AAH9sbUmYB_yuMCPdSSZrvWNMatRq-XNtq8") 
+    API_ID = os.environ.get("API_ID", "15529802")
+    API_HASH = os.environ.get("API_HASH", "92bcb6aa798a6f1feadbc917fccb54d3")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7513781272:AAE-B3XnkFG9QA23sPaEOowySzxGMJ4ABLI") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://pegawov487:wHVVNX9OIVtG7Ka1@cluster0.vvdig26.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6062527012').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://2gbRenameAUG:2gbRenameAUG@cluster0.uaysx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "XeonForward")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '821215952').split()]
 
 
 class temp(object): 
